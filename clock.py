7fe637d7d01e1f89ea3650a1226f09360f9a0c9e
Jake Gluck <jagluck@terpmail.umd.edu>
2019-11-19 21:34:41 -0500

    every 5 min

--- clock.py
+++ clock.py
@@ -3,21 +3,21 @@ from bot import *
 
 sched = BlockingScheduler()
 
-# @sched.scheduled_job('interval', minutes=5)
-# def timed_job():
-#     print("start search")
-#     cityName = "Washington, DC, US"
-#     cityId = "1409"
-#     days = 1
-#     runBot(days,cityName,cityId)
-
-@sched.scheduled_job('cron', hour=8)
-def scheduled_job():
-    print("it is 8am - start search")
+@sched.scheduled_job('interval', minutes=5)
+def timed_job():
+    print("start search")
     cityName = "Washington, DC, US"
     cityId = "1409"
     days = 1
     runBot(days,cityName,cityId)
+
+# @sched.scheduled_job('cron', hour=8)
+# def scheduled_job():
+#     print("it is 8am - start search")
+#     cityName = "Washington, DC, US"
+#     cityId = "1409"
+#     days = 1
+#     runBot(days,cityName,cityId)
               
 print("start script")
 sched.start()
